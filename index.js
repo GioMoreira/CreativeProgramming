eca6172eaac955def443b660fa57a6a619528957
Giovanna Moreira - LDB <gi.moreira@plaenge.com.br>
2022-09-06 15:07:56 -0300

    Comportamento randomizado + loop e condicional

--- index.js
+++ index.js
@@ -18,22 +18,45 @@ let canva1 = document.getElementById('canva1');
         context1.arc(190, 150, 100, 0, Math.PI * 2); // Parametros: X, Y, Radius, Angulo inicial, Algulo final (em radianos)
         context1.stroke();
 
+
+        const width = 50;
+        const height = 50;
+        const gap = 30;
+        let x,y;
+
         for(let i = 0; i < 6; i++){ 
-            let width = 50;
-            let height = 50;
-            let gap = 30;
-            let x = 100 + (width + gap) * i;
-            let y = 100;
-            
-            
-            context2.beginPath();
-            context2.rect(x, y, width, height);    
-             if(i % 2 == 0){
-                 context2.fillStyle = 'pink'
-                 context2.fillRect(x, y, width, height); 
-             } else if( i != 0 & i % 2 != 0 ){
-                 context2.stroke();
-                 
+            for(let j = 0; j < 5; j++){
+                x = 100 + (width + gap) * i;
+                y = 100 + (height + gap) * j;
+                
+                
+                context2.beginPath();
+                context2.rect(x, y, width, height);    
+                if((i % 2 == 0 & j%2==0) | (i % 2 != 0 & j%2!=0)){
+                    context2.fillStyle = 'pink'
+                    context2.fillRect(x, y, width, height); 
+
+                    context2.beginPath();
+                    context2.rect(x + 8, y + 8, width - 16, height - 16 );
+                    if(Math.random() < 0.5){
+                        context2.beginPath();
+                        context2.rect(x + 8, y + 8, width - 16, height - 16 );
+                        context2.stroke();
+                    }
+                    
+                } 
+                else{
+                    context2.stroke();      
+                    if(Math.random() > 0.5){
+                        context2.beginPath();
+                        context2.rect(x + 8, y + 8, width - 16, height - 16 );
+                        context2.stroke();
+                    
+                    }              
+                }
+
             }
+
+            
+            
         }
-        
