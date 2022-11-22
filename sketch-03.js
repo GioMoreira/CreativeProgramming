77941187ecf448e0fb9ff2c0bde20aa806fa0fec
GioMoreira <sgiomoreira@gmail.com>
2022-11-21 23:36:32 -0300

    Animation - constelations

--- sketch-03.js
+++ sketch-03.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
@@ -16,7 +17,7 @@ const animate = () => {
 const sketch = ({ context, width, height }) => {
   const agents = [];
 
-  for(i = 0; i < 70; i++) {
+  for(i = 0; i < 40; i++) {
     const x = random.range(0, width);
     const y = random.range(0, height) ;
     agents.push(new Agent(x, y));
@@ -24,8 +25,28 @@ const sketch = ({ context, width, height }) => {
 
 
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < agents.length; i++) {
+      const agent = agents[i];
+
+      for(let j = i+1; j < agents.length; j++){
+        const other = agents[j];
+
+        const dist = agent.pos.getDistance(other.pos);
+
+        if (dist > 200) continue;
+
+        context.lineWidth = math.mapRange(dist, 0, 200, 8, 0.5);
+
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.strokeStyle = 'white';
+        context.stroke();
+      }
+    }
 
     agents.forEach(agent => {
       agent.update();
@@ -41,6 +62,13 @@ class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+  }
+
+  getDistance(v) {
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+    return Math.sqrt(dx * dx + dy * dy);
+    
   }
 }
 
@@ -62,7 +90,7 @@ class Agent {
   }
 
   draw(context) {
-    context.fillStyle = 'lightsteelblue';
+    context.fillStyle = 'white';
 
     context.save();
     context.translate(this.pos.x, this.pos.y);
@@ -71,6 +99,7 @@ class Agent {
     context.beginPath();
     context.arc(0, 0, this.radius, 0, Math.PI*2);
     context.fill();
+    context.strokeStyle = 'steelBlue';
     context.stroke();
 
     context.restore();
